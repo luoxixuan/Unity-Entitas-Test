0d83a42e0e067a5b3e7a6777d8ef2e90392813df
luoxixuan <luoxixuan@outlook.com>
2019-05-08 16:30:18 +0800

    init

---
+++ Assets/SampleTest/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var contexts = new Contexts();
+
+        var e = contexts.game.CreateEntity();
+
+        e.AddHealth(100);
+    }
+    
+}
